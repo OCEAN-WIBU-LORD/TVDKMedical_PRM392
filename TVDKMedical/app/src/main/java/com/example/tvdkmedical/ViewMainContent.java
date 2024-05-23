54a8eab0dc3799c5120b3320e31296a2e83762d0
OCEAN <dinhduong1492003@gmail.com>
2024-05-23 21:19:27 +0700

    Update btn LogOut - DuongDDHE176496
    Update btn LogOut -DuongDDHE176496

--- TVDKMedical/app/src/main/java/com/example/tvdkmedical/ViewMainContent.java
+++ TVDKMedical/app/src/main/java/com/example/tvdkmedical/ViewMainContent.java
@@ -1,6 +1,11 @@
 package com.example.tvdkmedical;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +13,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.button.MaterialButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class ViewMainContent extends AppCompatActivity {
+    MaterialButton buttonLogOut;
+    FirebaseAuth mAuth;
+    ProgressBar progressBar;
+    TextView logOut, profileId;
+    FirebaseUser userDetails;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +34,29 @@ public class ViewMainContent extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        mAuth =FirebaseAuth.getInstance();
+        logOut = findViewById(R.id.logOut);
+        profileId = findViewById(R.id.profileId);
+        userDetails = mAuth.getCurrentUser();
+        if(userDetails == null){
+            Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+            startActivity(intent);
+            finish();
+        }else{
+            profileId.setText(userDetails.getEmail());
+        }
+
+        logOut.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+                Toast.makeText(getApplicationContext(),"Log Out successfully", Toast.LENGTH_SHORT).show();
+                finish();
+            }
         });
     }
 }
