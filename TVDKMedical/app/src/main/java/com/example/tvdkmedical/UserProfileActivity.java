9a4fb39cd2a994f61f68f0ee9e8f4d76d1d1012f
OCEAN <dinhduong1492003@gmail.com>
2024-06-12 12:56:53 +0700

    Dinh Dai Duong, Commit code for User Profile.
    User Profile Code commit (part-1) - DuongDDHE176496

--- TVDKMedical/app/src/main/java/com/example/tvdkmedical/UserProfileActivity.java
+++ TVDKMedical/app/src/main/java/com/example/tvdkmedical/UserProfileActivity.java
@@ -1,20 +1,38 @@
 package com.example.tvdkmedical;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+
 public class UserProfileActivity extends AppCompatActivity {
+    CardView editCardView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_user_profile);
+        editCardView = findViewById(R.id.editCardView);
+        editCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create an Intent to start LoginActivity
+                Intent intent = new Intent(UserProfileActivity.this, Fragm.class);
+
+                // Start the LoginActivity
+                startActivity(intent);
+            }
+        });
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
