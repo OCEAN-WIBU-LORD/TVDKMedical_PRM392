4eced1f1ad7b04c45342d1ab09149076c73dcf16
OCEAN <dinhduong1492003@gmail.com>
2024-06-14 11:22:46 +0700

    UpdateViewMain Content Activity -DuongDDHE176496
    DuongDDHE176496 - Commit Update Fixed Error App Crashing during  Firebase Data retrieve exception.

--- TVDKMedical/app/src/main/java/com/example/tvdkmedical/ViewMainContent.java
+++ TVDKMedical/app/src/main/java/com/example/tvdkmedical/ViewMainContent.java
@@ -1,9 +1,8 @@
 package com.example.tvdkmedical;
 
-import static com.example.tvdkmedical.R.id.postlist;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -25,7 +24,6 @@ import com.example.tvdkmedical.models.Appointment;
 import com.example.tvdkmedical.models.Post;
 import com.example.tvdkmedical.views.appointment.AppointmentDetailsActivity;
 import com.google.android.material.button.MaterialButton;
-import com.google.firebase.Timestamp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -33,7 +31,6 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.google.type.DateTime;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -44,10 +41,11 @@ import java.util.Locale;
 import java.util.Objects;
 
 public class ViewMainContent extends AppCompatActivity {
+    private static final String TAG = "ViewMainContent";
     MaterialButton buttonLogOut;
     FirebaseAuth mAuth;
     ProgressBar progressBar;
-    TextView logOut, profileId, doctorName,dateInfo,timeInfo;
+    TextView logOut, profileId, doctorName, dateInfo, timeInfo;
     ImageView doctorImage;
     FirebaseUser userDetails;
     Button btnProfile;
@@ -56,8 +54,6 @@ public class ViewMainContent extends AppCompatActivity {
     PostAdapter postAdapter;
     RecyclerView recyclerView;
     List<Post> data;
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -70,82 +66,95 @@ public class ViewMainContent extends AppCompatActivity {
             return insets;
         });
 
-        mAuth =FirebaseAuth.getInstance();
+        mAuth = FirebaseAuth.getInstance();
         logOut = findViewById(R.id.logOut);
-        //profileId = findViewById(R.id.profileId);
         appointmentDetailsBtn = findViewById(R.id.appointmentDetails);
-        doctorImage=findViewById(R.id.doctor_image);
-        doctorName =findViewById(R.id.doctor_name);
-        dateInfo=findViewById(R.id.date_info);
-        timeInfo=findViewById(R.id.time_info);
+        doctorImage = findViewById(R.id.doctor_image);
+        doctorName = findViewById(R.id.doctor_name);
+        dateInfo = findViewById(R.id.date_info);
+        timeInfo = findViewById(R.id.time_info);
 
         userDetails = mAuth.getCurrentUser();
-        if(userDetails == null){
-            Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+        if (userDetails == null) {
+            Log.e(TAG, "User is not authenticated, redirecting to login");
+            Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
             startActivity(intent);
             finish();
-        }else{
-           // profileId.setText(userDetails.getEmail());
+            return;
+        } else {
+            // profileId.setText(userDetails.getEmail());
         }
 
-        recyclerView=findViewById(R.id.postlist);
+        recyclerView = findViewById(R.id.postlist);
         database = FirebaseDatabase.getInstance().getReference().child("posts");
 
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         data = new ArrayList<>();
-        postAdapter=new PostAdapter(data,this);
+        postAdapter = new PostAdapter(data, this);
         recyclerView.setAdapter(postAdapter);
 
+        loadAppointments();
+        loadPosts();
+
+        logOut.setOnClickListener(view -> {
+            FirebaseAuth.getInstance().signOut();
+            Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+            startActivity(intent);
+            Toast.makeText(getApplicationContext(), "Log Out successfully", Toast.LENGTH_SHORT).show();
+            finish();
+        });
+
+        Button btnProfile1 = findViewById(R.id.btnProfile);
+        btnProfile1.setOnClickListener(v -> {
+            Intent intent = new Intent(ViewMainContent.this, FragmentUserProfile.class);
+            startActivity(intent);
+        });
+
+        appointmentDetailsBtn.setOnClickListener(v -> {
+            Intent intent = new Intent(ViewMainContent.this, AppointmentDetailsActivity.class);
+            startActivity(intent);
+        });
+    }
+
+    private void loadAppointments() {
         appointmentDatabase = FirebaseDatabase.getInstance().getReference().child("appointments");
         appointmentDatabase.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 if (snapshot.exists()) {
-                    Appointment nearestAppointment = new Appointment();
-                    long now = new Date().getTime();
-
+                    Appointment nearestAppointment = null;
                     for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
-                        String status = Objects.requireNonNull(dataSnapshot.child("status").getValue()).toString();
-                        String userId = Objects.requireNonNull(dataSnapshot.child("userId").getValue()).toString();
+                        if (dataSnapshot.hasChild("status") && dataSnapshot.hasChild("userId")) {
+                            String status = Objects.requireNonNull(dataSnapshot.child("status").getValue()).toString();
+                            String userId = Objects.requireNonNull(dataSnapshot.child("userId").getValue()).toString();
 
-                        if ("pending".equals(status) && userId.equals(userDetails.getUid())) {
-                            int startTime = Objects.requireNonNull(dataSnapshot.child("startTime").getValue(Integer.class));
-                            com.google.firebase.Timestamp startTs = new com.google.firebase.Timestamp(startTime, 0);
-                            int endTime = Objects.requireNonNull(dataSnapshot.child("endTime").getValue(Integer.class));
-                            com.google.firebase.Timestamp endTs = new com.google.firebase.Timestamp(endTime, 0);
-                           if (nearestAppointment == null || nearestAppointment.getStartTime() == null|| startTs.compareTo(nearestAppointment.getStartTime()) < 0) {
-                                String appointmentId =  Objects.requireNonNull(dataSnapshot.getKey().toString());
-                                String diseasedId =  Objects.requireNonNull(dataSnapshot.child("diseaseId").getValue()).toString();
-                                String doctorId = Objects.requireNonNull(dataSnapshot.child("doctorId").getValue()).toString();
-                                String note = Objects.requireNonNull(dataSnapshot.child("note").getValue()).toString();
+                            if ("pending".equals(status) && userId.equals(userDetails.getUid())) {
+                                Integer startTime = dataSnapshot.child("startTime").getValue(Integer.class);
+                                Integer endTime = dataSnapshot.child("endTime").getValue(Integer.class);
 
-                                nearestAppointment = new Appointment( appointmentId, diseasedId,doctorId, endTs, note, startTs, status, userId);
-                           }
+                                if (startTime != null && endTime != null) {
+                                    com.google.firebase.Timestamp startTs = new com.google.firebase.Timestamp(startTime, 0);
+                                    com.google.firebase.Timestamp endTs = new com.google.firebase.Timestamp(endTime, 0);
+
+                                    if (nearestAppointment == null || nearestAppointment.getStartTime() == null || startTs.compareTo(nearestAppointment.getStartTime()) < 0) {
+                                        String appointmentId = dataSnapshot.getKey();
+                                        String diseaseId = Objects.requireNonNull(dataSnapshot.child("diseaseId").getValue()).toString();
+                                        String doctorId = Objects.requireNonNull(dataSnapshot.child("doctorId").getValue()).toString();
+                                        String note = Objects.requireNonNull(dataSnapshot.child("note").getValue()).toString();
+
+                                        nearestAppointment = new Appointment(appointmentId, diseaseId, doctorId, endTs, note, startTs, status, userId);
+                                    }
+                                }
+                            }
+                        } else {
+                            Log.e(TAG, "Invalid dataSnapshot: " + dataSnapshot);
                         }
                     }
 
                     if (nearestAppointment != null) {
-                        DatabaseReference doctorDatabase = FirebaseDatabase.getInstance().getReference().child("doctors").child(nearestAppointment.getDoctorId());
-                        doctorDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
-                            @Override
-                            public void onDataChange(@NonNull DataSnapshot doctorSnapshot) {
-                                if (doctorSnapshot.exists()) {
-                                    String doctorNameStr = Objects.requireNonNull(doctorSnapshot.child("name").getValue()).toString();
-                                    doctorName.setText(doctorNameStr);
-                                }
-                            }
-
-                            @Override
-                            public void onCancelled(@NonNull DatabaseError error) {
-                                Toast.makeText(getApplicationContext(), "Failed to read doctor data from Firebase.", Toast.LENGTH_SHORT).show();
-                            }
-                        });
-                        Date startDate = nearestAppointment.getStartTime().toDate();
-                        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault());
-                        SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm", Locale.getDefault());
-                        dateInfo.setText(dateFormat.format(startDate));
-                        timeInfo.setText(timeFormat.format(startDate));
+                        fetchDoctorDetails(nearestAppointment);
+                        updateAppointmentDetails(nearestAppointment);
                     }
                 }
             }
@@ -153,31 +162,65 @@ public class ViewMainContent extends AppCompatActivity {
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
                 Toast.makeText(getApplicationContext(), "Failed to read data from Firebase.", Toast.LENGTH_SHORT).show();
+                Log.e(TAG, "Failed to read data from Firebase: " + error.getMessage());
             }
         });
+    }
+
+    private void fetchDoctorDetails(Appointment nearestAppointment) {
+        doctorDatabase = FirebaseDatabase.getInstance().getReference().child("doctors").child(nearestAppointment.getDoctorId());
+        doctorDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot doctorSnapshot) {
+                if (doctorSnapshot.exists()) {
+                    String doctorNameStr = Objects.requireNonNull(doctorSnapshot.child("name").getValue()).toString();
+                    doctorName.setText(doctorNameStr);
+                } else {
+                    Log.e(TAG, "Doctor data not found for ID: " + nearestAppointment.getDoctorId());
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(getApplicationContext(), "Failed to read doctor data from Firebase.", Toast.LENGTH_SHORT).show();
+                Log.e(TAG, "Failed to read doctor data from Firebase: " + error.getMessage());
+            }
+        });
+    }
+
+    private void updateAppointmentDetails(Appointment nearestAppointment) {
+        Date startDate = nearestAppointment.getStartTime().toDate();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault());
+        SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm", Locale.getDefault());
+        dateInfo.setText(dateFormat.format(startDate));
+        timeInfo.setText(timeFormat.format(startDate));
+    }
+
+    private void loadPosts() {
         database.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                for(DataSnapshot dataSnapshot :snapshot.getChildren()){
-                    Post post=new Post();
-                    //String i =Objects.requireNonNull(dataSnapshot.getKey().toString());
-                    post.setPost_id(Objects.requireNonNull(dataSnapshot.getKey()));
-                    post.setTitle(Objects.requireNonNull(dataSnapshot.child("title").getValue().toString()));
-                    post.setContent(Objects.requireNonNull(dataSnapshot.child("content").getValue().toString()));
-                    post.setAuthor(Objects.requireNonNull(dataSnapshot.child("author").getValue().toString()));
-                 //   post.setCategory(Objects.requireNonNull(dataSnapshot.child("category").getValue().toString()));
-                    post.setCategory("abc");
-                    String date =Objects.requireNonNull(dataSnapshot.child("date").getValue().toString());
-                    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-                    Date dateformat=null;
-                    try {
-                        dateformat=dateFormat.parse(date);
-                    } catch (ParseException e) {
-                        throw new RuntimeException(e);
-                    }
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    if (dataSnapshot.exists() && dataSnapshot.hasChild("title") && dataSnapshot.hasChild("content") && dataSnapshot.hasChild("author") && dataSnapshot.hasChild("date")) {
+                        Post post = new Post();
+                        post.setPost_id(Objects.requireNonNull(dataSnapshot.getKey()));
+                        post.setTitle(Objects.requireNonNull(dataSnapshot.child("title").getValue()).toString());
+                        post.setContent(Objects.requireNonNull(dataSnapshot.child("content").getValue()).toString());
+                        post.setAuthor(Objects.requireNonNull(dataSnapshot.child("author").getValue()).toString());
+                        post.setCategory("abc"); // Static category since it's commented out
 
-                    post.setDate(dateformat);
-                    data.add(post);
+                        String date = Objects.requireNonNull(dataSnapshot.child("date").getValue()).toString();
+                        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
+                        try {
+                            Date dateformat = dateFormat.parse(date);
+                            post.setDate(dateformat);
+                        } catch (ParseException e) {
+                            Log.e(TAG, "Date parsing error: " + e.getMessage());
+                        }
+                        data.add(post);
+                    } else {
+                        Log.e(TAG, "Invalid post dataSnapshot: " + dataSnapshot);
+                    }
                 }
                 postAdapter.notifyDataSetChanged();
             }
@@ -185,39 +228,7 @@ public class ViewMainContent extends AppCompatActivity {
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
                 Toast.makeText(getApplicationContext(), "Failed to read data from Firebase.", Toast.LENGTH_SHORT).show();
-
-            }
-        });
-
-        logOut.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view) {
-                FirebaseAuth.getInstance().signOut();
-                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
-                startActivity(intent);
-                Toast.makeText(getApplicationContext(),"Log Out successfully", Toast.LENGTH_SHORT).show();
-                finish();
-            }
-        });
-        Button btnProfile1 = findViewById(R.id.btnProfile);
-
-        // Set OnClickListener to the button
-        btnProfile1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Create an Intent to start LoginActivity
-                Intent intent = new Intent(ViewMainContent.this, UserProfileActivity.class);
-
-                // Start the LoginActivity
-                startActivity(intent);
-            }
-        });
-
-        appointmentDetailsBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(ViewMainContent.this, AppointmentDetailsActivity.class);
-                startActivity(intent);
+                Log.e(TAG, "Failed to read data from Firebase: " + error.getMessage());
             }
         });
     }
