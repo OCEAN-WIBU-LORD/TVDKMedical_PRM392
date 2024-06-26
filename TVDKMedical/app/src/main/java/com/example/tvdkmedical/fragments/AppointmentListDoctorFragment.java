898b957a106800cf87a1fe7170ce4d7680b214d2
Builanviet <builanviet@gmail.com>
2024-06-26 11:55:15 +0700

    appointmentlist fragment added

---
+++ TVDKMedical/app/src/main/java/com/example/tvdkmedical/fragments/AppointmentListDoctorFragment.java
@@ -0,0 +1,23 @@
+package com.example.tvdkmedical.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.tvdkmedical.R;
+
+class AppointmentListDoctorFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_appointment_list_doctor, container, false);
+    }
+}
